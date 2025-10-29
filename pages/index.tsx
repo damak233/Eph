75a7caaac1ac1e0517621e3543f18ac6c53bc6d5
damak233 <ephemeralartco@gmail.com>
2025-10-29 14:38:48 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo, useState, useEffect } from 'react'
+import SEO from "../components/SEO";
 import SEO from '../components/SEO'   // 🆕 SEO komponens
 
 export default function Home() {
