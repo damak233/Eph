2ead41aa2479342b7af301483ad6a3defbcca7dc
damak233 <ephemeralartco@gmail.com>
2025-10-29 16:29:00 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+// pages/_app.tsx
+import '../Styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
