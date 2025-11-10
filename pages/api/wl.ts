7d592faba15ae78ea96996edd6f106f11a2f7922
damak233 <ephemeralartco@gmail.com>
2025-11-10 20:43:44 +0100

    Replace getSupabase with supabase import

--- pages/api/wl.ts
+++ pages/api/wl.ts
@@ -1,6 +1,6 @@
 // /pages/api/wl.ts
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { getSupabase } from '../../lib/supabaseClient'
+import { supabase } from '../../lib/supabaseClient'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
